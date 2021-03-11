0a8cbff5350d297767729a1247a6c907a696824e
Bruth98 <16bruth.stem@gmail.com>
2021-03-11 10:00:00 -0500

    Changing Admin- Brady

--- Admin.java
+++ Admin.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Admin extends User{
+public class Admin extends Officer{
     public Admin() {
         super();
     }
@@ -39,31 +39,6 @@ public class Admin extends User{
     }
 
     public void editEvidence(String description, boolean fingerprints, boolean weapon, boolean drugs, String location, ArrayList<String> witnesses) {
-
-    }
-
-    public void addSuspect(String name, String address, int age, int dob, String description,
-                           String gender, String race, String hairColor, double height, double weight,
-                           boolean tattoo, boolean criminalRecord, ArrayList<String> familyMembers) {
-
-                           }
-
-    public void addWitness(String name, String address, int age, int dob, String description, 
-                           ArrayList<String> incidentReport, boolean relationToVictim) {
-
-    }
-
-    public void addVictim(String name, String address, int age, int dob, String description,
-                          ArrayList<String> incidentReport, boolean alive, int phoneNum) {
-
-    }
-
-    public void addCase(Crime crime, ArrayList<Suspect> suspects, ArrayList<Witness> witnesses, 
-    ArrayList<Victim> victims, ArrayList<Evidence> evidence, int caseID, int date, String location) {
-
-    }
-
-    public void addEvidence(String description, boolean fingerprints, boolean weapon, boolean drugs, String location, ArrayList<String> witnesses) {
 
     }
 }
