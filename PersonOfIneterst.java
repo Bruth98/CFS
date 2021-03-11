99f52e8488f8d1a64d63671edc1c0f1685795ceb
Bruth98 <16bruth.stem@gmail.com>
2021-03-11 10:14:56 -0500

    Adding P.O.I.- Brady

---
+++ PersonOfIneterst.java
@@ -0,0 +1,47 @@
+public class PersonOfIneterst extends Person {
+    public String gender;
+    public String race;
+    public String hairColor;
+    public double height;
+    public double weight;
+    public boolean tattoo;
+    public boolean criminalRecord;
+
+    public PersonOfIneterst() {
+        super();
+    }
+
+    public PersonOfIneterst(String gender, String race, String hairColor, double height, 
+                            double weight, boolean tattoo, boolean criminalRecord) {
+        super();
+    }
+
+    public String getGender() {
+        return "";
+    }
+    
+    public String getRace() {
+        return "";
+    }
+
+    public String getHairColor() {
+        return "";
+    }
+
+    public double getHeight() {
+        return 0.0;
+    }
+
+    public double getWeight() {
+        return 0.0;
+    }
+
+    public boolean hasTattoo() {
+        return false;
+    }
+
+    public boolean hasCriminalRecord() {
+        return false;
+    }
+}
+
