fb7f0c4b7d18cea4fa7c7ef40d26e650cea8e2e5
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-03-11 10:48:00 -0500

    Added Victim

---
+++ Victim.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class Victim {
+    private ArrayList<String> incidentReport;
+    private boolean alive;
+    private int phoneNum;
+
+    public Victim(){
+
+    }
+
+    public Victim(String name, String address, int age, Date dob, String Description, ArrayList<String> incidentReport, boolean alive, int phoneNum){
+
+    }
+
+    public ArrayList<String> getIncidentReport(){
+
+    }
+
+    public boolean getStatus(){
+ 
+    }
+
+    public int getPhoneNum(){
+        
+    }
+}
