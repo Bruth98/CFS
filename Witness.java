a08c49e10abd4e9cd5c235f535ded8ee693ce2e1
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-03-11 12:20:48 -0500

    Added Witness

---
+++ Witness.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class Witness {
+    private ArrayList<String> incidentReport;
+    private boolean relationToVictim;
+
+    public Witness(){
+
+    }
+
+    public Witness(String name, String address, int age, Date dob, String description, ArrayList<String> incidentReport, boolean relationToVictim){
+
+    }
+
+    public ArrayList<String> getIncidentReport(){
+
+    }
+
+    public boolean getRelation(){
+        
+    }
+}
