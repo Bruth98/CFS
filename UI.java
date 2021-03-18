99a5c0f0d2a53ce4af6a76fdf735474d4b5fa3ee
AustinDOdell <adodell@email.sc.edu>
2021-03-18 12:58:41 -0400

    UI commit

--- UI.java
+++ UI.java
@@ -1,28 +1,62 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class UI {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        boolean loggedIn = false;
-        System.out.println("****************************************");
-        System.out.println("Welcome to Crime Fighter Software!");
-        System.out.println("Please enter your User ID: ");
-        int userID = keyboard.nextInt();
-
-        System.out.println("Please enter your Password: ");
-        String password = keyboard.next();
-
-        System.out.println("****************************************");
-        System.out.println(userID);
-        System.out.println(password);
+        UI Ui = new UI();
+        Ui.play();
     }
-
 public void play(){
+    Scanner keyboard = new Scanner(System.in);
+    boolean loggedIn = false;
+    System.out.println("****************************************");
+    System.out.println("Welcome to Crime Fighter Software!");
+    System.out.println("Please enter your User ID: ");
+    int userID = keyboard.nextInt();
+
+    System.out.println("Please enter your Password: ");
+    String password = keyboard.next();
+
+    System.out.println("****************************************");
+    System.out.println(userID);
+    System.out.println(password);
    User user =  User.getInstance();
    System.out.println("Current Users");
+   
+    System.out.println("***** Users Online *****");
+    displayUsers();
 
+    while(addUser()) {
+        int employeeID = getFieldInt("employeeID");
+        String password = getField("password");
 
+        User.addUser(employeeID, password);
+    }
+
+    System.out.println("***** Your updated list of Users *****")
+    displayUsers();
+
+    System.out.println("\nHave a good day!")
+}
+private String getField(String prompt) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print(prompt + ": ");
+    return keyboard.next();
+}
+private int getFieldInt(String prompt) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print(prompt + ": ");
+    return keyboard.nextInt();
+}
+private boolean addUser() {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("Would you like to add a User? (Y or N):");
+
+    String input = keyboard.next();
+    if(input.toLowerCase().trim().equals("y")) return true;
+    return false;
 }
 public void displayUsers(){
-    User user = User.getInstance();
-    ArrayList<User> users = user.getPeople();
+    User user2 = User.getInstance();
+    ArrayList<User> users = user2.getUser();
 }
+    }
