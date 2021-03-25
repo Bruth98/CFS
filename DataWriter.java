29249f5e6fddcd24872907c770eae95ab9bb6744
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-03-25 12:02:08 -0400

    Finished dataWriter

--- DataWriter.java
+++ DataWriter.java
@@ -107,6 +107,117 @@ public class DataWriter extends DataConstants {
 
         return crimeDetails;
     }
+
+    public static void saveEvidence(){
+        Evidences evidences = Evidences.getInstance();
+        ArrayList<Evidence> evidence = evidences.getEvidence();
+        JSONArray jsonEvidence = new JSONArray();
+
+        for(int i=0; i<evidence.size(); i++){
+            jsonEvidence.add(getEvidenceJSON(evidence.get(i)));
+        }
+        try(FileWriter file = new FileWriter(EVIDENCE_FILE_NAME)){
+            file.write(jsonEvidence.toJSONString());
+            file.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getEvidenceJSON(Evidence evidence){
+        JSONObject evidenceDetails = new JSONObject();
+        evidenceDetails.put(EVIDENCE_DESCRIPTION, evidence.getDescription());
+        evidenceDetails.put(EVIDENCE_DRUGS, evidence.getDrugs());
+        evidenceDetails.put(EVIDENCE_FINGERPRINTS, evidence.getFingerPrints());
+        evidenceDetails.put(EVIDENCE_LOACTION, evidence.getLocation());
+        evidenceDetails.put(EVIDENCE_WEAPON, evidence.getWeapon());
+
+        return evidenceDetails;
+    }
+
+    public static void saveOfficers(){
+        Officers officers = Officers.getInstance();
+        ArrayList<Officer> officer = officers.getOfficers();
+        JSONArray jsonOfficers = new JSONArray();
+
+        for(int i=0; i<officers.size(); i++){
+            jsonOfficers.add(getOfficerJSON(officer.get(i)));
+        }
+        try(FileWriter file = new FileWriter(OFFICER_FILE_NAME)){
+            file.write(jsonOfficers.toJSONString());
+            file.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getOfficerJSON(Officer officer){
+        JSONObject officerDetails = new JSONObject();
+        officerDetails.put(OFFICER_DEPARTMENT, officer.getDepartment());
+        officerDetails.put(OFFICER_FIRST_NAME, officer,getFirstName());
+        officerDetails.put(OFFICER_LAST_NAME, officer.getLastName());
+
+        return officerDetails;
+    }
+
+    public static void saveSuspects(){
+        Suspects suspects = Suspects.getInstance();
+        ArrayList<Suspect> suspect = suspects.getSuspects();
+        JSONArray jsonSuspect = new JSONArray();
+
+        for(int i=0; i<suspect.size(); i++){
+            jsonSuspect.add(getSuspectJSON(suspect.get(i)));
+        }
+        try(FileWriter file = new FileWriter(SUSPECT_FILE_NAME)){
+            file.write(jsonSuspect.toJSONString());
+            file.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getSuspectJSON(Suspect suspect){
+        JSONObject suspectDetails = new JSONObject();
+        suspectDetails.put(SUSPECT_CRIMINAL_RECORD, suspect.getCriminalRecord());
+        suspectDetails.put(SUSPECT_GENDER, suspect.getGender());
+        suspectDetails.put(SUSPECT_HAIR_COLOR, suspect.getHairColor());
+        suspectDetails.put(SUSPECT_HEIGHT, suspect.getHeight());
+        suspectDetails.put(SUSPECT_RACE, suspect.getRace());
+        suspectDetails.put(SUSPECT_TATTOO, suspect.getTattoo());
+        suspectDetails.put(SUSPECT_WEIGHT, suspect.getWeight());
+        
+
+        return suspectDetails;
+    }
+
+    public static void saveVictims(){
+        Victims victims = Victims.getInstance();
+        ArrayList<Victim> victim = victims.getVictims();
+        JSONArray jsonVictim = new JSONArray();
+
+        for(int i=0; i<victim.size(); i++){
+            jsonVictim.add(getVictimJSON(victim.get(i)));
+        }
+        try(FileWriter file = new FileWriter(VICTIM_FILE_NAME)){
+            file.write(jsonVictim.toJSONString());
+            file.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getVictimJSON(Victim victim){
+        JSONObject victimDetails = new JSONObject();
+        victimDetails.put(VICTIM_ALIVE, victim.getAlive());
+        victimDetails.put(VICTIM_PHONE_NUMBER, victim.getPhoneNum());
+        
+      
+        return victimDetails;
+    }
     public static void main(String[] args){
         DataWriter.saveUsers();
     }
