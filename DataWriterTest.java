0238ca98c071d460635846f410bd2a58af8cf075
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-04-10 12:41:11 -0400

    tested first user ID and password writer

--- DataWriterTest.java
+++ DataWriterTest.java
@@ -4,9 +4,15 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 public class DataWriterTest {
+    private static Users users = Users.getInstance();
+    private static ArrayList<User> userList = users.getUsers();
+
     @BeforeClass
     public static void oneTimeSetUp(){
 
@@ -17,16 +23,23 @@ public class DataWriterTest {
 
     }
 
-    @Before
+    @BeforeEach
     public static void setup(){
         //runs before each test
-    }
+        userList.clear();
+        userList.add(new User(00, "abcd"));
+        userList.add(new User(01, "efgh"));
+        DataWriter.saveUsers();
 
-    @After
+
+    }   
+
+    @AfterEach
     public static void teardown(){
         //runs at the end of each test
+        Users.getInstance().getUsers().clear();
+        DataWriter.saveUsers();
     }
-
     //assertEquals(val1,val2)
     //assertFalse(val)
     //assertTrue(val)
@@ -35,5 +48,29 @@ public class DataWriterTest {
     //asserNull(val)
     //assertNotNull(val)
 
+    @Test
+    void testGetUsersSize(){
+        userList = DataLoader.loadUsers();
+        assertEquals(2, userList.size());
+    }
+
+    @Test
+    void testGetUsersSizeZero(){
+        Users.getInstance().getUsers().clear();
+        DataWriter.saveUsers();
+        assertEquals(0,userList.size());
+    }
+
+    @Test
+    void testGetUserFirstUserID(){
+        userList = DataLoader.loadUsers();
+        assertEquals(00, userList.get(0).getUserID());
+}
+
+    @Test
+    void testGetUserFirstUserPassword(){
+        userList = DataLoader.loadUsers();
+        assertEquals("abcd", userList.get(1).getPassword());
+    }
     
 }
