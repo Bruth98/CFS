e98e8724d05f7118071a8575342306ad70fcb70d
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-04-10 12:19:50 -0400

    Basic setup for jUnit tester for dataWriter

--- DataWriterTest.java
+++ DataWriterTest.java
@@ -1,6 +1,39 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 public class DataWriterTest {
+    @BeforeClass
+    public static void oneTimeSetUp(){
+
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown(){
+
+    }
+
+    @Before
+    public static void setup(){
+        //runs before each test
+    }
+
+    @After
+    public static void teardown(){
+        //runs at the end of each test
+    }
+
+    //assertEquals(val1,val2)
+    //assertFalse(val)
+    //assertTrue(val)
+    //assertSame(val1,val2)
+    //assertNotSame(val1,val2)
+    //asserNull(val)
+    //assertNotNull(val)
+
     
 }
