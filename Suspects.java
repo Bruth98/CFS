83289ebfbf1609e785f7ebb6d76863dedcb8e637
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 14:21:44 -0400

    Update Suspects.java

--- Suspects.java
+++ Suspects.java
@@ -1,8 +1,15 @@
+/* 
+This is functionally the same file as Cases, Crimes, Persons, Witnesses, and Victims.
+It both reads and writes to our JSON Database
+*/
 import java.util.ArrayList;
 public class Suspects {
     private static Suspects suspects = null;
     private static ArrayList<Suspect> suspectList = new ArrayList<Suspect>();
 
+    /*
+    Load the list of saved suspects from the JSON database
+    */
     private Suspects() {
         //suspectList = DataLoader.loadSuspect();
     }
@@ -18,6 +25,9 @@ public class Suspects {
         return suspectList;
     }
     
+    /*
+   Adds a suspect to the list if they are not already in the database
+    */
     public boolean addSuspect(String name, String address, int dob,
       int age, String description, String gender, String race, String hairColor,
       double height, double weight, String tattoo, boolean criminalRecord, long phoneNum) {
@@ -28,7 +38,9 @@ public class Suspects {
           race, hairColor, height, weight, tattoo, criminalRecord,age, phoneNum));
           return true;
       }
-
+    /*
+    Checks to see if the suspect is alreadu in the database\
+    */
       public boolean haveSuspect(String name, int dob) {
           for (Suspect suspect : suspectList) {
               if (suspect.getName().equals(name) && suspect.getDOB() == dob) {
@@ -37,7 +49,9 @@ public class Suspects {
           }
           return false;
       }
-
+    /*
+    Save the new list of Suspects in JSON format to our database
+    */
       public void saveSuspects() {
           DataWriter.saveSuspects();
       }
