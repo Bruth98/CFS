4ed73d423b7500d8d1e2392657db568fddbe8f4f
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 14:13:50 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -3,6 +3,9 @@ import java.util.Collection;
 
 public class Database {
 
+/*
+starts with a chunk of getters
+*/
     public static ArrayList<User> getUsers() {
         return new ArrayList<User>();
     }
@@ -31,31 +34,40 @@ public class Database {
         return new ArrayList<Witness>();
     }
 
-    public static ArrayList<Person> getFirst(String name) {
+    /*
+    Search for a person with a specific name
+    */
+    public static ArrayList<Person> getFirst(String name) {  // Search for a person with a specific first name
         ArrayList<Person> person = new ArrayList<Person>();
-        for(int i=0; i < Persons.getInstance().getPerson().size();i++){
-            Person names = Persons.getInstance().getPerson().get(i);
-            if(names.getName().equals(name)){
-                return Persons.getInstance().getPerson();
+        for(int i=0; i < Persons.getInstance().getPerson().size();i++){  // loop over the size of the ArrayList
+            Person names = Persons.getInstance().getPerson().get(i);  // get the name of person (i) in the list
+            if(names.getName().equals(name)){  // compare that name to the name being searched for
+                return Persons.getInstance().getPerson();  // if they match, return that person from the list
             }
         }
-        System.out.println("Sorry no suspects with that name.");
-        return null;
+        System.out.println("Sorry no suspects with that name.");  // if the names do not match
+        return null;  // return nothing
     }
-
-    public static ArrayList<Person> getAge(int age) {
+	
+    /*
+    Almost exactly the same as the getFirst(String name) method
+    */
+    public static ArrayList<Person> getAge(int age) {  // Search for a person with a specific age
         ArrayList<Person> person = new ArrayList<Person>();
-        for(int i=0; i < Persons.getInstance().getPerson().size();i++){
-            Person names = Persons.getInstance().getPerson().get(i);
-            if(names.getAge() == age){
-                return Persons.getInstance().getPerson();
+        for(int i=0; i < Persons.getInstance().getPerson().size();i++){  // loop over the size of the ArrayList
+            Person names = Persons.getInstance().getPerson().get(i);  // Eastablish that person as a new object
+            if(names.getAge() == age){  // if that person's age matches the one being searched for
+                return Persons.getInstance().getPerson();  // return that person to the user
             }
         }
         System.out.println("Sorry no suspects with that age.");
         return null;
     }
-
-    public static ArrayList<PersonOfInterest> getHeight(double height) {
+	
+    /*
+    Almost exactly the same as the getFirst(String name) method
+    */
+    public static ArrayList<PersonOfInterest> getHeight(double height) {  // Search for a person with a specific height
         ArrayList<PersonOfInterest> person = new ArrayList<PersonOfInterest>();
         for(int i=0; i < PersonsOfInterest.getInstance().getPerson().size();i++){
             PersonOfInterest names = PersonsOfInterest.getInstance().getPerson().get(i);
@@ -67,7 +79,10 @@ public class Database {
         return null;
     }
 
-    public static ArrayList<PersonOfInterest> getWeight(double weight) {
+    /*
+    Almost exactly the same as the getFirst(String name) method
+    */
+    public static ArrayList<PersonOfInterest> getWeight(double weight) {  // Search for a person with a specific weight
         ArrayList<PersonOfInterest> person = new ArrayList<PersonOfInterest>();
         for(int i=0; i < Persons.getInstance().getPerson().size();i++){
             PersonOfInterest names = PersonsOfInterest.getInstance().getPerson().get(i);
@@ -79,7 +94,10 @@ public class Database {
         return null;
     }
 
-    public static ArrayList<PersonOfInterest> getGender(String gender) {
+    /*
+    Almost exactly the same as the getFirst(String name) method
+    */
+    public static ArrayList<PersonOfInterest> getGender(String gender) {  // Search for a person with a specific gender
         ArrayList<PersonOfInterest> person = new ArrayList<PersonOfInterest>();
         for(int i=0; i < PersonsOfInterest.getInstance().getPerson().size();i++){
             PersonOfInterest names = PersonsOfInterest.getInstance().getPerson().get(i);
@@ -103,7 +121,11 @@ public class Database {
         return null;
     }
 */
-    public static ArrayList<PersonOfInterest> getTattooDescription(String tattoo) {
+    
+    /*
+    Almost exactly the same as the getFirst(String name) method
+    */
+    public static ArrayList<PersonOfInterest> getTattooDescription(String tattoo) {  // // Search for a person with a specific tattoo description
         ArrayList<PersonOfInterest> person = new ArrayList<PersonOfInterest>();
         for(int i=0; i < Persons.getInstance().getPerson().size();i++){
             PersonOfInterest names = PersonsOfInterest.getInstance().getPerson().get(i);
@@ -115,6 +137,9 @@ public class Database {
         return null;
     }
 
+    /*
+    Almost exactly the same as the getFirst(String name) method
+    */
     public static ArrayList<PersonOfInterest> getHairColor(String hairColor) {
         ArrayList<PersonOfInterest> person = new ArrayList<PersonOfInterest>();
         for(int i=0; i < PersonsOfInterest.getInstance().getPerson().size();i++){
