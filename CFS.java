77cb1f985194c7a5c636ec03d6a2f21bed641a2c
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 14:27:33 -0400

    Update CFS.java

--- CFS.java
+++ CFS.java
@@ -1,3 +1,7 @@
+/*
+This class links our UI with the JSON database so that all the variables in a case can be properly loaded, checked against, and added if needed.
+*/
+
 public class CFS {
     private Users users;
     private Cases cases;
@@ -8,6 +12,9 @@ public class CFS {
     private User currentUser;
     private static Database database;
     
+    /*
+    create an instance of the database
+    */
     public CFS() {
         users = Users.getInstance();
         cases = Cases.getInstance();
@@ -17,10 +24,16 @@ public class CFS {
         victims = Victims.getInstance();
     }
 
+    /*
+    Add a user to the database
+    */
     public boolean createAccount(int userID, String password) {
         return users.addUser(userID, password);
     }
 
+    /*
+    Check to see if the entered log in information is stored in the database
+    */
     public boolean login(int userID, String password) {
         if (!users.haveUser(userID, password)) {
             return false;
@@ -28,14 +41,23 @@ public class CFS {
         return true;
     }
 
+    /*
+    Search the database to see if a specific case ID exists
+    */
     public boolean findCases(int caseID) {
         return cases.haveCase(caseID);
     }
-
+    
+    /*
+    Search the database to return a Case with a specific case ID
+    */
     public Case getCase(int caseID) {
         return cases.getCase(caseID);
     }
 
+    /*
+    Saves all updates to the database
+    */
     public void logout() {
         users.saveUsers();
         cases.saveCases();
